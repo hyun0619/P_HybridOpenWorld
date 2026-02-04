ac5675cdaac0496e335d23b52389ec367421bacd
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-02-04 19:39:08 +0900

    언리얼 셋팅

---
+++ Source/HybridOpenWorld/HybridOpenWorld.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HybridOpenWorld.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HybridOpenWorld, "HybridOpenWorld" );
+
+DEFINE_LOG_CATEGORY(LogHybridOpenWorld)
+ 
