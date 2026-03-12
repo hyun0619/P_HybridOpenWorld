5c86e0042a4e025824ebca0702ce0100c4b22dea
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-03-12 16:50:40 +0900

    260312 환경 서브 시스템 - 시간 흐름 계산

---
+++ Source/HybridOpenWorld/Private/EnvironmentSubsystem.cpp
@@ -0,0 +1,58 @@
+﻿#include "EnvironmentSubsystem.h"
+
+#include "ProjectHGameInstance.h"
+
+void UEnvironmentSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	
+	CurrentTime = StartingTime; // 기본 설정값으로 시간 초기화
+	
+	// 서브시스템 초기화 시점에 World가 아직 생성되지 않았을 경우를 대비한 안전 장치
+	if (UProjectHGameInstance* GI = Cast<UProjectHGameInstance>(GetGameInstance()))
+	{
+		if (GI->PlayerData.SavedTime >= 0.0f)
+		{
+			CurrentTime = GI->PlayerData.SavedTime;
+		}
+		
+		GI->GetTimerManager().SetTimer( // Tick 의존 X, 전용 타이머 사용하여 CPU 점유율 고정
+			TimerUpdateTimerHandle,
+			this,
+			&UEnvironmentSubsystem::OnTimerUpdate,
+			TimeUpdateInterval,
+			true);
+	}
+}
+
+void UEnvironmentSubsystem::Deinitialize()
+{
+	// 서브시스템 종료 시 타이머를 명확히 해제하여 메모리 누수 방지
+	if (UGameInstance* GI = GetGameInstance())
+	{
+		GI->GetTimerManager().ClearTimer(TimerUpdateTimerHandle);
+	}
+	
+	Super::Deinitialize();
+}
+
+void UEnvironmentSubsystem::OnTimerUpdate()
+{
+	// 시간이 흐르도록 하는 코드
+	CurrentTime += TimeFlowSpeed;
+	if (CurrentTime >= 24.0f) CurrentTime = 0.0f;
+
+	if (UProjectHGameInstance* GI = Cast<UProjectHGameInstance>(GetGameInstance()))
+	{
+		// 실시간으로 변하는 시간을 GameInstance에 계속 동기화
+		GI->PlayerData.SavedTime = CurrentTime;
+	}
+	
+	//추후 프리셋 보간 로직 호출 예정
+}
+
+void UEnvironmentSubsystem::UpdateEnvironmentLerp()
+{
+}
+
+
---
+++ Source/HybridOpenWorld/Public/EnvironmentSubsystem.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "EnvironmentSubsystem.generated.h"
+
+class UEnvPresetDataAsset;
+/**
+ * 시간의 흐름과 날씨 변화 계산하는 환경 관리 서브시스템
+ */
+UCLASS()
+class HYBRIDOPENWORLD_API UEnvironmentSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override; // 서브시스템 생성 시 실행
+	virtual void Deinitialize() override; // 서브시스템 파괴 시 실행
+	
+	// 게임 시간을 수동으로 설정하거나 가져오는 함수
+	UFUNCTION(BlueprintCallable, Category="Environment")
+	void SetCurrentTime(float NewTime) {CurrentTime = NewTime;}
+	UFUNCTION(BlueprintPure, Category="Environment")
+	float GetCurrentTime() const {return CurrentTime;}
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Environment|Settings")
+	float TimeUpdateInterval = 0.1f; // 타이머 실행 간격 (0.1초 = 초당 10번 연산)
+	UPROPERTY(EditDefaultsOnly, Category = "Environment|Settings")
+	float TimeFlowSpeed = 0.01f; // 시간이 흐르는 속도
+	UPROPERTY(EditDefaultsOnly, Category = "Environment|Settings")
+	float StartingTime = 9.0f; // 게임 시작 시 기본 시작 시간 (오전 9시)
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Environment|Presets")
+	TArray<UEnvPresetDataAsset*> EnvironmentPresets; // 시간대별 프리셋들을 리스트로 관리
+	
+private:
+	void OnTimerUpdate(); // 주기적으로 실행하여 환경 보간 등 처리
+	FTimerHandle TimerUpdateTimerHandle;
+	
+	void UpdateEnvironmentLerp(); // 두 개의 프리셋 - 현재 프리셋, 다음 프리셋을 찾는 함수
+	
+	UPROPERTY()
+	float CurrentTime; // 흐르는 시간을 저장하는 변수 (현재 시간)
+};
