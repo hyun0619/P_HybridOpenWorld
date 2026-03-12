19e2476288a895b19cc3398bc45a2a017c5ae9c9
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-03-12 16:49:47 +0900

    260312 레벨 데이터 셋팅

---
+++ Source/HybridOpenWorld/Private/LevelDataAsset.cpp
@@ -0,0 +1,1 @@
+﻿#include "LevelDataAsset.h"
---
+++ Source/HybridOpenWorld/Public/LevelDataAsset.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelDataAsset.generated.h"
+
+/**
+ * 레벨 정보
+ */
+UENUM(BlueprintType)
+enum class ELevelType : uint8 { WorldMap, Detailed, Event };
+
+UCLASS(BlueprintType)
+class HYBRIDOPENWORLD_API ULevelDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, Category="Level")
+	ELevelType LevelType;
+	
+	// 직접적인 UWorld* 참조는 데이터 에셋 로드 시 맵 전체를 메모리에 끌어올려 렉 유발
+	UPROPERTY(EditAnywhere, Category="Level")
+	TSoftObjectPtr<UWorld> LevelReference;
+	
+	UPROPERTY(EditAnywhere, Category="Level")
+	FString LevelName;
+	
+	UPROPERTY(EditAnywhere, Category="Spawn")
+	FVector DefaultSpawnLocation;
+};
