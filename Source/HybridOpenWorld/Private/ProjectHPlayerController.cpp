b8cd099eb4ab1f4f28892f449c0bde3d636e49b5
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-03-16 00:32:39 +0900

    260316 플레이어컨트롤러 - 레벨별 카메라 엑터 자동 연결

---
+++ Source/HybridOpenWorld/Private/ProjectHPlayerController.cpp
@@ -0,0 +1,26 @@
+﻿#include "ProjectHPlayerController.h"
+#include "ProjectHCameraActor.h"
+#include "Kismet/GameplayStatics.h"
+
+
+void AProjectHPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+}
+
+void AProjectHPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// 레벨에 배치된 전용 카메라 액터 자동 연결
+	TArray<AActor*> FoundCameras;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AProjectHCameraActor::StaticClass(), FoundCameras);
+	
+	if (FoundCameras.Num() > 0)
+	{
+		MainCameraActor = Cast<AProjectHCameraActor>(FoundCameras[0]);
+		SetViewTarget(MainCameraActor); // 찾은 카메라로 화면 연결
+	}
+	
+	// 초기 카메라 배정 로직 (나중에 Spawn이나 FindActor 등으로 구현 필요)
+}
---
+++ Source/HybridOpenWorld/Public/ProjectHPlayerController.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ProjectHPlayerController.generated.h"
+
+
+class AProjectHCameraActor;
+
+/**
+ * 플레이어 입력 처리, 레벨별 카메라 결정
+ */
+UCLASS()
+class HYBRIDOPENWORLD_API AProjectHPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void PlayerTick(float DeltaTime) override;
+	
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY()
+	AProjectHCameraActor* MainCameraActor; // 현재 제어 중인 카메라 참조
+};
