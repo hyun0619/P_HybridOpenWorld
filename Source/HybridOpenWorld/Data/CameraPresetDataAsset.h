e53218415f7392b44b610d92c72ded8acd7f57eb
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-03-16 20:20:55 +0900

    260316 카메라 프리셋 데이터 에셋 생성 - 레벨별 셋팅 위함

---
+++ Source/HybridOpenWorld/Data/CameraPresetDataAsset.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CameraPresetDataAsset.generated.h"
+
+/**
+ * 레벨별 카메라 프리셋 데이터
+ */
+UCLASS(BlueprintType)
+class HYBRIDOPENWORLD_API UCameraPresetDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, Category="Camera|Settings")
+	float TargetArmLength = 1000.f; // 타겟 거리
+	
+	UPROPERTY(EditAnywhere, Category = "Camera Settings")
+	float FieldOfView = 90.0f; // FOV
+	
+	UPROPERTY(EditAnywhere, Category = "Camera Settings")
+	FRotator Rotation = FRotator(-30.f, 0.f, 0.f); // 회전값
+	
+	UPROPERTY(EditAnywhere, Category = "Camera Settings")
+	float TrackingInterpSpeed = 5.0f; // 카메라가 캐릭터를 따라가는 속도
+};
