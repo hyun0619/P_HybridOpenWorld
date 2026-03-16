a5568788fc8107f48c1963ea5c8d665a4d66c57e
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-03-16 22:11:54 +0900

    260316 모든 레벨 데이터를 관리하는 레벨 마스터 에셋 생성

---
+++ Source/HybridOpenWorld/Data/LevelMasterAsset.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelMasterAsset.generated.h"
+
+class ULevelDataAsset;
+/**
+ * 프로젝트의 모든 레벨 데이터를 한곳에서 관리하는 마스터 에셋
+ */
+UCLASS()
+class HYBRIDOPENWORLD_API ULevelMasterAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	// 모든 레벨 데이터 에셋
+	UPROPERTY(EditAnywhere, Category = "Levels")
+	TArray<ULevelDataAsset*> AllLevelDatas;
+};
