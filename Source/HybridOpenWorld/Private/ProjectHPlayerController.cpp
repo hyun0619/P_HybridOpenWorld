90186d90591647d180f49d08405450c1dd3f1c2b
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-03-16 14:04:37 +0900

    260316 레벨별 이동 구현 - WASD, 마우스

--- Source/HybridOpenWorld/Private/ProjectHPlayerController.cpp
+++ Source/HybridOpenWorld/Private/ProjectHPlayerController.cpp
@@ -1,11 +1,17 @@
 ﻿#include "ProjectHPlayerController.h"
-#include "ProjectHCameraActor.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+#include "ProjectHCameraActor.h" 
 #include "Kismet/GameplayStatics.h"
 
 
-void AProjectHPlayerController::PlayerTick(float DeltaTime)
+AProjectHPlayerController::AProjectHPlayerController()
 {
-	Super::PlayerTick(DeltaTime);
+	// 마우스 기본 설정
+	bShowMouseCursor = true;
+	bEnableClickEvents = true;
+	bEnableMouseOverEvents = true;
 }
 
 void AProjectHPlayerController::BeginPlay()
@@ -15,7 +21,6 @@ void AProjectHPlayerController::BeginPlay()
 	// 레벨에 배치된 전용 카메라 액터 자동 연결
 	TArray<AActor*> FoundCameras;
 	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AProjectHCameraActor::StaticClass(), FoundCameras);
-	
 	if (FoundCameras.Num() > 0)
 	{
 		MainCameraActor = Cast<AProjectHCameraActor>(FoundCameras[0]);
@@ -23,4 +28,75 @@ void AProjectHPlayerController::BeginPlay()
 	}
 	
 	// 초기 카메라 배정 로직 (나중에 Spawn이나 FindActor 등으로 구현 필요)
+	
+	SetInputModeByType(true); // 초기 입력 모드 설정
+}
+
+void AProjectHPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	// Enhanced Input 컴포넌트로 캐스팅하여 바인딩
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		// WASD 이동 (Triggered: 계속 누르고 있을 때)
+		EnhancedInputComponent->BindAction(IA_Move_KeyBoard, ETriggerEvent::Triggered, this, &AProjectHPlayerController::HandleMove_KeyBoard);
+		// 마우스 이동 (Started: 클릭한 순간)
+		EnhancedInputComponent->BindAction(IA_Move_MouseClick, ETriggerEvent::Started, this, &AProjectHPlayerController::HandleMove_MouseClick);
+		/*추후 추가될 기능들 자리*/
+	}
+}
+
+void AProjectHPlayerController::SetInputModeByType(bool bIsWorldMap)
+{
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->ClearAllMappings(); // 기존 모든 매핑을 한번에 정리
+		
+		if (IMC_Global) Subsystem->AddMappingContext(IMC_Global, 0); // 공통 기능은 최우선으로
+		
+		if (IMC_WorldMap)
+		{
+			// 월드맵 모드 - 마우스 이동 활성화
+			if (IMC_WorldMap) Subsystem->AddMappingContext(IMC_WorldMap, 1);
+			bShowMouseCursor = true;
+			// 카메라 설정 변경
+			if (MainCameraActor) MainCameraActor->SetCameraMode(true);
+		}
+		else
+		{
+			// 세부지역 모드 - WASD 이동 활성화
+			if (IMC_Detailed) Subsystem->AddMappingContext(IMC_Detailed, 1);
+			bShowMouseCursor = false;
+			if (MainCameraActor) MainCameraActor->SetCameraMode(false);
+		}
+	}
+}
+
+void AProjectHPlayerController::HandleMove_KeyBoard(const FInputActionValue& Value)
+{
+	// Detailed 모드에서만 작동함
+	FVector2D MoveVector = Value.Get<FVector2D>();
+	if (APawn* ControlledPawn = GetPawn())
+	{
+		// 카메라의 전방 방향을 기준으로 이동
+		ControlledPawn->AddMovementInput(FVector::ForwardVector, MoveVector.Y);
+		ControlledPawn->AddMovementInput(FVector::RightVector, MoveVector.X);
+	}
+}
+
+void AProjectHPlayerController::HandleMove_MouseClick()
+{
+	// WorldMap 모드에서만 작동함 (IMC에 의해 필터링됨)
+	FHitResult Hit;
+	if (GetHitResultUnderCursor(ECC_Visibility, true, Hit))
+	{
+		// 내비게이션 시스템을 이용해 클릭 지점으로 자동 이동
+		UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, Hit.ImpactPoint);
+	}
+}
+
+void AProjectHPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
 }
--- Source/HybridOpenWorld/Public/ProjectHPlayerController.h
+++ Source/HybridOpenWorld/Public/ProjectHPlayerController.h
@@ -2,10 +2,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "InputActionValue.h"
 #include "ProjectHPlayerController.generated.h"
 
 
 class AProjectHCameraActor;
+class UInputMappingContext;
+class UInputAction;
 
 /**
  * 플레이어 입력 처리, 레벨별 카메라 결정
@@ -16,10 +19,33 @@ class HYBRIDOPENWORLD_API AProjectHPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
+	AProjectHPlayerController();
 	virtual void PlayerTick(float DeltaTime) override;
+	
+	void SetInputModeByType(bool bIsWorldMap); // 조작 모드 변경 (true: 월드맵/마우스, false: 세부지역/WASD)
 	
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Input|Context")
+	UInputMappingContext* IMC_Global; // 항상 켜있는 기능 (인벤토리, 지도 등)
+	UPROPERTY(EditDefaultsOnly, Category="Input|Context")
+	UInputMappingContext* IMC_WorldMap; // 월드맵 전용 - 마우스 이동
+	UPROPERTY(EditDefaultsOnly, Category="Input|Context")
+	UInputMappingContext* IMC_Detailed; // 세부지역 전용 - WASD 이동
+	
+	UPROPERTY(EditDefaultsOnly, Category="Input|Action")
+	UInputAction* IA_Move_KeyBoard; // 키보드 이동
+	UPROPERTY(EditDefaultsOnly, Category="Input|Action")
+	UInputAction* IA_Move_MouseClick; // 마우스 클릭 이동 
+	/*추후 추가될 기능들 자리*/
+	
+private:
+	// 입력 핸들러 - 필요한 시점에만 호출
+	void HandleMove_KeyBoard(const FInputActionValue& Value);
+	void HandleMove_MouseClick();
+	/*추후 추가될 기능들 자리*/
 	
 	UPROPERTY()
 	AProjectHCameraActor* MainCameraActor; // 현재 제어 중인 카메라 참조
