83ecb6cfa0805959bc5d494ab6adf1612687ba77
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-03-18 01:35:56 +0900

    260318 레벨마스터에셋 -> 게임마스터에셋으로 이름 변경

---
+++ Source/HybridOpenWorld/Data/GameMasterAsset.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameMasterAsset.generated.h"
+
+class ULevelDataAsset;
+/**
+ * 프로젝트의 모든 레벨 데이터를 한곳에서 관리하는 마스터 에셋
+ */
+UCLASS()
+class HYBRIDOPENWORLD_API UGameMasterAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, Category="Settings", meta=(DisplayName="레벨 정보 테이블"))
+	UDataTable* LevelTable;
+};
