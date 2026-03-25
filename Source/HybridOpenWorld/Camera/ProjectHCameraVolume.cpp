b20e4c436762198be25436eda6bce954cf5c0ec6
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-03-25 21:54:10 +0900

    260325 카메라 볼륨

---
+++ Source/HybridOpenWorld/Camera/ProjectHCameraVolume.cpp
@@ -0,0 +1,50 @@
+﻿#include "ProjectHCameraVolume.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Pawn.h"
+#include "ProjectHCameraSubsystem.h" // 매니저 헤더
+#include "Data/CameraPresetDataAsset.h"    // 데이터 에셋 헤더
+
+AProjectHCameraVolume::AProjectHCameraVolume()
+{
+	PrimaryActorTick.bCanEverTick = false; // 볼륨은 연산이 필요 없으므로 끕니다.
+
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+	RootComponent = CollisionBox;
+    
+	// 트리거 설정 (물리 충돌은 없고 겹침만 감지)
+	CollisionBox->SetCollisionProfileName(TEXT("Trigger"));
+
+	// 이벤트 바인딩
+	CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &AProjectHCameraVolume::OnOverlapBegin);
+	CollisionBox->OnComponentEndOverlap.AddDynamic(this, &AProjectHCameraVolume::OnOverlapEnd);
+}
+
+void AProjectHCameraVolume::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AProjectHCameraVolume::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	// 닿은 액터가 플레이어인지 확인
+	APawn* PlayerPawn = Cast<APawn>(OtherActor);
+	if (PlayerPawn && PlayerPawn->IsPlayerControlled())
+	{
+		if (UProjectHCameraSubsystem* Subsystem = GetWorld()->GetSubsystem<UProjectHCameraSubsystem>())
+		{
+			Subsystem->PushCameraPreset(CameraPreset, Priority);
+		}
+	}
+}
+
+void AProjectHCameraVolume::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	APawn* PlayerPawn = Cast<APawn>(OtherActor);
+	if (PlayerPawn && PlayerPawn->IsPlayerControlled())
+	{
+		if (UProjectHCameraSubsystem* Subsystem = GetWorld()->GetSubsystem<UProjectHCameraSubsystem>())
+		{
+			Subsystem->PopCameraPreset(CameraPreset);
+		}
+	}
+}
---
+++ Source/HybridOpenWorld/Camera/ProjectHCameraVolume.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProjectHCameraVolume.generated.h"
+
+class UBoxComponent;
+class UCameraPresetDataAsset;
+
+UCLASS()
+class HYBRIDOPENWORLD_API AProjectHCameraVolume : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AProjectHCameraVolume();
+
+protected:
+	virtual void BeginPlay() override;
+
+	// 플레이어가 들어오고 나갈 때 감지하는 함수
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+protected:
+	// 에디터에서 영역을 조절할 박스 컴포넌트
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UBoxComponent* CollisionBox;
+
+	// 이 구역에서 사용할 카메라 설정 데이터
+	UPROPERTY(EditAnywhere, Category = "Camera Setup")
+	UCameraPresetDataAsset* CameraPreset;
+
+	// 볼륨이 겹칠 때 어떤 뷰를 우선할지 (높을수록 우선)
+	UPROPERTY(EditAnywhere, Category = "Camera Setup")
+	int32 Priority = 0;
+};
