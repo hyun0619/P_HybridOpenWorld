a8477c7aacbf8474470f8822168493fd2cd9001e
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-04-07 23:08:44 +0900

    test01

--- Source/HybridOpenWorld/Camera/ProjectHCameraSubsystem.cpp
+++ Source/HybridOpenWorld/Camera/ProjectHCameraSubsystem.cpp
@@ -66,7 +66,7 @@ bool UProjectHCameraSubsystem::GetActivePreset(FCameraPresetSettings& OutSetting
 		{
 			OutSettings = Volume->GetCameraSettings();
 			return true;
-		}
+		} //aaaa
 		OutSettings = CameraStack.Last().Settings;
 		return true;
 	}
